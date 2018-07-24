0c7edf29bbc54308e8517a03eed75025b7ec861a
Monica Tamaru Jones <mntamaru@gmail.com>
2018-07-24 22:27:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
   
+  return drivers.filter(function(driver) {
+    if (driver['revenue'] > revenue) {
+      return driver;
+    }
+  });
 }
